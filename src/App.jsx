fa247ed2e07770812847e107c8115010077be35b
Nasrin Mamizadeh <nasriiin2016@gmail.com>
2025-01-17 09:51:55 +0330

    Convert Svg Icon to Jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Footer from "./components/modules/footer/Footer";
 import Header from "./components/modules/header/Header";
+import SvgComponents from "./components/svgComponents/svgComponents";
 import Banner from "./components/templates/index/Banner";
 import Categories from "./components/templates/index/Categories";
-
 function App() {
   return (
     <>
+    <SvgComponents />
       <Header />
       <main>
         <Banner />
