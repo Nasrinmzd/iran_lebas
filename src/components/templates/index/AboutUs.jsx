6a9851be86d0eca65509418623c5b21897389d41
Nasrin Mamizadeh <nasriiin2016@gmail.com>
2025-02-11 11:19:49 +0330

    Add CategoriCart Section to the Project

--- src/components/templates/index/AboutUs.jsx
+++ src/components/templates/index/AboutUs.jsx
@@ -2,7 +2,7 @@ import Button from "../../../ui/Button";
 
 function AboutUs() {
   return (
-    <section className="flex justify-center items-center mb-[150px]  bg-[url(/images/bgabout.png),_url(/images/bgabout1.png)] bg-no-repeat bg-[position:left,_right]">
+    <section className="flex justify-center items-center mb-[150px] bg-[url(/images/bgabout.png),_url(/images/bgabout1.png)] bg-no-repeat bg-[position:left,_right]">
       <div className="w-[1072px] h-[381px] flex justify-center items-center">
         <img className="w-[381px] h-[381px] ml-8" src="/images/aboutus.png" alt="" />
         <div>
--- src/components/templates/index/CategoriCart.jsx
+++ src/components/templates/index/CategoriCart.jsx
@@ -1,9 +1,29 @@
+import ProductsCart from "../ProductsCart";
+
 function CategoriCart() {
-    return (
-        <div>
-            
-        </div>
-    )
+  const categoriCartData = {
+    title: "تیشرت مردانه آستین بلند",
+    img: "/images/blooz.png",
+  };
+
+  return (
+    <section className="flex justify-center mb-[150px] mt-[250px] gap-[70px] bg-[url(/images/bgpink.png),_url(/images/bgblue1.png)] bg-[center_top_1rem] bg-repeat-round">
+      <div className="relative bottom-[169px]">
+        <ProductsCart
+          title={categoriCartData.title}
+          img={categoriCartData.img}
+        />
+      </div>
+      <ProductsCart title={categoriCartData.title} img={categoriCartData.img} />
+      <div className="relative bottom-[169px]">
+        <ProductsCart
+          title={categoriCartData.title}
+          img={categoriCartData.img}
+        />
+      </div>
+      <ProductsCart title={categoriCartData.title} img={categoriCartData.img} />
+    </section>
+  );
 }
 
-export default CategoriCart
+export default CategoriCart;
