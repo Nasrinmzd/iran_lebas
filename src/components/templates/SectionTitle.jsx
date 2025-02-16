6cc1ef5a557b30d15c68523e5d14fdb284d5dcb2
Nasrin Mamizadeh <nasriiin2016@gmail.com>
2025-02-16 08:33:24 +0330

    Updates

--- src/components/templates/SectionTitle.jsx
+++
@@ -1,9 +0,0 @@
-function SectionTitle({children}) {
-  return (
-    <div className="w-full flex items-center justify-center font-bold text-2xl leading-9 text-primary pb-10">
-      {children}
-    </div>
-  );
-}
-
-export default SectionTitle;
